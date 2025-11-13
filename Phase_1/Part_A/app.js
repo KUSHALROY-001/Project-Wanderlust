48d10d62e5a4270ff464c406c0bb5c7052793b25
KUSHALROY-001 <kushalroy212006@gmail.com>
2025-11-14 00:41:29 +0530

    creating utils folder for error handling and also creating error.ejs to add some styling on error page

--- Phase_1/Part_A/app.js
+++ Phase_1/Part_A/app.js
@@ -6,6 +6,9 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const CustomError = require("./utils/CustomError.js");
+const { wrap } = require("module");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -26,100 +29,132 @@ main()
     console.log("Unable to connect to Mongodb : ", err);
   });
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.send("It is the index Route");
 });
 
 // All Listing Route
-app.get("/listings", (req, res) => {
-  Listing.find()
-    .then((allListing) => {
-      res.render("listing.ejs", { allListing });
-    })
-    .catch((err) => {
-      res.send("Error in Showing All Listings: ", err);
-      console.log(err);
-    });
-});
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
+    Listing.find()
+      .then((allListing) => {
+        res.render("listing.ejs", { allListing });
+      })
+      .catch((err) => {
+        res.send("Error in Showing All Listings: ", err);
+        console.log(err);
+      });
+  })
+);
 
 // Show route
-app.get("/listing/:id", (req, res) => {
-  let { id } = req.params;
-  Listing.findById(id)
-    .then((listing) => {
-      res.render("show.ejs", { listing });
-    })
-    .catch((err) => {
-      res.send("Error in Show Individual Listing : ", err);
-    });
-});
+app.get(
+  "/listing/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    Listing.findById(id)
+      .then((listing) => {
+        res.render("show.ejs", { listing });
+      })
+      .catch((err) => {
+        res.send("Error in Show Individual Listing : ", err);
+      });
+  })
+);
 
 // New Route
 app.get("/listings/new", (req, res) => {
   res.render("new.ejs");
 });
 
-app.post("/listing/new", (req, res) => {
-  // let { title, description, image, price, location, country } = req.body;
-  // let data = {
-  //   title: title,
-  //   description: description,
-  //   image: image,
-  //   price: price,
-  //   location: location,
-  //   country: country,
-  // };
+app.post(
+  "/listing/new",
+  wrapAsync(async (req, res, next) => {
+    // let { title, description, image, price, location, country } = req.body;
+    // let data = {
+    //   title: title,
+    //   description: description,
+    //   image: image,
+    //   price: price,
+    //   location: location,
+    //   country: country,
+    // };
+    if (!req.body.listing) {
+      throw new CustomError(400, "Invalid Listing Data");
+    }
 
-  Listing.insertOne(req.body.listing)
-    .then((result) => {
-      console.log(result);
-      res.redirect("/listings");
-    })
-    .catch((err) => {
-      res.send(
-        "Please fill the blanks otherwise no data will be inserted",
-        err
-      );
-      console.log(err);
-    });
-});
+    Listing.create(req.body.listing)
+      .then((result) => {
+        console.log(result);
+        res.redirect("/listings");
+      })
+      .catch((err) => {
+        next(new CustomError(400, "Invalid Listing Data"));
+      });
+  })
+);
 
 // Edit Route
-app.get("/listing/edit/:id", (req, res) => {
-  let { id } = req.params;
-  Listing.findById(id).then((listing) => {
-    res.render("edit.ejs", { listing });
-  });
-});
-
-app.put("/listing/edit/:id", (req, res) => {
-  let { id } = req.params;
-  Listing.findByIdAndUpdate(
-    id,
-    { $set: req.body.listing },
-    { runValidators: true },
-    { new: true }
-  )
-    .then((result) => {
-      res.redirect(`/listing/${id}`);
-    })
-    .catch((err) => {
-      console.log("error find in updation", err);
+app.get(
+  "/listing/edit/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    Listing.findById(id).then((listing) => {
+      res.render("edit.ejs", { listing });
     });
-});
+  })
+);
+
+app.put(
+  "/listing/edit/:id",
+  wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+      throw new CustomError(400, "Invalid Listing Data");
+    }
+
+    let { id } = req.params;
+    Listing.findByIdAndUpdate(
+      id,
+      { $set: req.body.listing },
+      { new: true, runValidators: true }
+    )
+      .then((result) => {
+        res.redirect(`/listing/${id}`);
+      })
+      .catch((err) => {
+        console.log("error find in updation", err);
+      });
+  })
+);
 
 // Delete Route
-app.delete("/listing/delete/:id", (req, res) => {
-  let { id } = req.params;
-  Listing.findByIdAndDelete(id)
-    .then((result) => {
-      console.log(result)
-      res.redirect("/listings");
-    })
-    .catch((err) => {
-      res.send("Error in Deletion");
-      console.log(err);
-    });
+app.delete(
+  "/listing/delete/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    Listing.findByIdAndDelete(id)
+      .then((result) => {
+        console.log(result);
+        res.redirect("/listings");
+      })
+      .catch((err) => {
+        res.send("Error in Deletion");
+        console.log(err);
+      });
+  })
+);
+
+// Catch-all for unmatched routes. Use `app.use` instead of `app.all("*", ...)`
+// to avoid path-to-regexp parsing errors for bare `*` patterns.
+app.use((req, res, next) => {
+  next(new CustomError(404, "Page Not Found"));
+});
+// ========== MiddleWare =========
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something Went Wrong" } = err;
+  console.log(message);
+  res.status(statusCode).render("error.ejs", { message });
 });
 
 app.listen(8080, () => {
