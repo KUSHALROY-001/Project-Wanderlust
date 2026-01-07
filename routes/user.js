3e8c9dbbb96ae17bd2cd9342679839471a85a823
KUSHALROY-001 <kushalroy212006@gmail.com>
2026-01-07 15:19:49 +0530

    Server-side signup form and validation

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const User = require("../models/user.js");
 
+// Signup Route
 router.get("/signup", (req, res) => {
   res.render("signup.ejs");
 });
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res, next) => {
+    try {
+      const { username, email, password } = req.body;
+      const newUser = new User({ username, email });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/listings");
+    } catch (err) {
+      req.flash("error", err.message);
+      res.redirect("/signup");
+    }
+  })
+);
 
 module.exports = router;
