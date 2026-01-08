1f2bd1d56e9d8a9a17012fc5d700fb114fd2722b
KUSHALROY-001 <kushalroy212006@gmail.com>
2026-01-08 18:26:25 +0530

    Add styling for login, signup, logout

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  // console.log(res.session);
+  res.locals.currUser = req.user;
   next();
 });
 
