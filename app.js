33a658d699371881af42b897f7e0665c7e45db6f
KUSHALROY-001 <kushalroy212006@gmail.com>
2026-01-14 23:27:22 +0530

    Using MULTER for file uploadation and CLODINARY for cloud storage to store the file

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
 const express = require("express");
 const mongoose = require("mongoose");
 const ejs = require("ejs");
@@ -14,6 +17,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
 const CustomError = require("./utils/CustomError.js");
+const multer = require("multer"); /* We can't directly extract the file from the "multipart/form-data" enctype so we use multer package for that*/
+const upload = multer({ dest: "uploads/" });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
